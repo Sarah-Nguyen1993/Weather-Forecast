956adf81527d817cc2952dd2cb32b70f88796f29
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-04 12:54:03 -0500

    updated script.js

--- script.js
+++ script.js
@@ -11,52 +11,56 @@
 //when the search button is clicked
     //how to make the search works either by clicking button or hitting enter
     $("#today").hide();
+    var API_Key = "0d0f0152212d1d78d1fa67d26aa4056b";
 $("#search-btn").click(function ()
 { 
     //why it does not work when vars are outside of the function
     var city = $("#input-box").val();
-    var API_Key = "0d0f0152212d1d78d1fa67d26aa4056b";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + API_Key;
+    var queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + API_Key;
+    var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city +"&cnt=5&units=imperial&appid=" + API_Key;
     var cityEl = $(".city-search-history");
     var todayWeatherDisplay = $("#today");
+    var fiveDayWeatherDisplay = $("#forecast");
+
+    //request info to get today weather
     $("#today").show();
     $.ajax({
-        url: queryURL,
+        url: queryURL1,
         method: "GET"
-    }).then(function(response) 
+    }).then(function(response1) 
     { 
-        console.log(response)
-        console.log(city)
+        console.log(response1)
         cityEl.append(
             $("<button class ='city-btn'>").text(city.charAt(0).toUpperCase() + city.slice(1))
         );
-
-        var temp = response.main.temp
-        var humid = response.main.humidity
-        var windSpeed = response.wind.speed 
-        //var uvIndex;
-
-        var city = {
-            name: city,
-            temperature: temp,
-            humidity: humid,
-            wind: windSpeed
-        };
        
+        //display today weather 
         todayWeatherDisplay.append(
-             //city name and date
+            //city name and date
             "<h1>" + city.charAt(0).toUpperCase() + city.slice(1) + "   " + "(" + moment().format('l') + ")",
             //temp
-            "<p>" + "Temperature: " + response.main.temp + " F", 
-             //humidity
-            "<p>" + "Humidity: " + response.main.humidity + " %",
+            "<p>" + "Temperature: " + response1.main.temp + " F", 
+            //humidity
+            "<p>" + "Humidity: " + response1.main.humidity + " %",
             //wind speed  
-            "<p>" + "Wind speed: " + response.wind.speed + " mph", 
+            "<p>" + "Wind speed: " + response1.wind.speed + " mph", 
             //uv index
-            ) 
-      //  localStorage.setItem('cities', JSON.stringify(cities))      
+            )    
 
     })
+
+    
+    $.ajax({
+       url: queryURL2,
+        method: "GET"
+       }).then( function(response2){
+           console.log(response2)
+           console.log(queryURL2)
+
+       })
+   
+       //var uvIndex;
+ //  localStorage.setItem('cities', JSON.stringify(cities))      
 })
 
 // function fiveDayDisplay(){
